4384bc8efdc034d6a03371028b19038696b3346c
jlauza <lauzajayson@gmail.com>
2023-10-22 21:55:10 +0800

    new files and switch route

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
-Test
+<?php
+
+function showError404() {
+    header("HTTP/1.1 404 Not Found");
+    require '../app/views/404/404.php';
+    exit; // Important, to stop further processing
+}
+
+$request = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$request = trim($request,"/");
+// $request = str_replace("","/", $request);
+
+switch ($request) {
+    case "":
+        case"home":
+        require_once "./home.php";
+        break;
+
+        case "dashboard":
+        require_once "../app/views/dashboard.php";
+        break;
+    
+    default:
+            showError404();
+            break;
+}
