be539331f6689a21196f6b4a4c8add1cb72041e4
jlauza <lauzajayson@gmail.com>
2025-06-27 02:06:57 +0800

    rebuild docker

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '../../controller/UserController.php';
+require_once __DIR__ . '/../../controller/UserController.php';
+require_once __DIR__ . '/../models/users/UserModel.php';
 
 $controller = new UserController();
 $error = '';
