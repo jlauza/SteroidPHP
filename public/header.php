160d0ccd31e0c6aaf6fdbe6bbde738a00439866e
jlauza <lauzajayson@gmail.com>
2025-06-29 23:15:53 +0800

    created homepage layout

--- public/header.php
+++ public/header.php
@@ -3,9 +3,13 @@
         <title>Steroid PHP</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Open this if you are working with custom CSS -->
         <!-- <link rel="stylesheet" type="text/css" href="css/dark-mode.css"> -->
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- In your main layout -->
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
 
-<body class="dark-mode">
+<body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
--- public/home.php
+++ public/home.php
@@ -1,7 +1,18 @@
 <div class="min-h-screen flex items-center justify-center">
     <div class="text-center">
-        <h1 class="text-4xl font-bold mb-4 text-blue-600">Welcome to SteroidPHP</h1>
+        <h1 class="text-4xl font-bold mb-4">Welcome to SteroidPHP</h1>
         <p class="text-lg mb-6">This turtle is on roids. MVC never looked this fast.</p>
-        <a href="/products" class="inline-block bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition">View Products</a>
+        <a href="#" class="inline-block bg-green-800 text-white px-6 py-2 rounded hover:bg-green-700 transition">Get Started</a>
+
+        <div class="mt-8 flex justify-center space-x-4">
+            <a href="#" class="">
+                <i class="fab fa-github fa-2x"></i>
+                <p class="">Follow us on GitHub</p>
+            </a>
+        </div>
     </div>
+</div>
+<div class="text-center text-gray-500">
+<small>&copy; <?php echo date("Y"); ?> SteroidPHP. All rights reserved.</small>
+<small>Created by Jayson Lauza (InitiaDev)</small>    
 </div>
