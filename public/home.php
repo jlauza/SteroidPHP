d8bbd79df655b280d10f0c9e9d8762bb90e26a10
jlauza <lauzajayson@gmail.com>
2025-06-29 23:42:57 +0800

    added logo

--- public/home.php
+++ public/home.php
@@ -1,8 +1,9 @@
 <div class="min-h-screen flex items-center justify-center">
     <div class="text-center">
+        <img src="image/logo.png" alt="SteroidPHP Logo" class="mx-auto mb-4 w-30" />
         <h1 class="text-4xl font-bold mb-4">Welcome to SteroidPHP</h1>
         <p class="text-lg mb-6">This turtle is on roids. MVC never looked this fast.</p>
-        <a href="#" class="inline-block bg-green-800 text-white px-6 py-2 rounded hover:bg-green-700 transition">Get Started</a>
+        <a href="#" class="inline-block bg-green-800 text-white px-6 py-2 rounded hover:bg-green-700 transition">Get Started <i class="fa-solid fa-arrow-right-long"></i></a>
 
         <div class="mt-8 flex justify-center space-x-4">
             <a href="#" class="">
